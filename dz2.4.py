e73acf1cb30eec6b8c6661144b39e7c562de1ede
ElenaZamyatina <elenzamyatina12@gmail.com>
2023-01-05 11:49:53 +0400

    2.4 решение подглядела

--- dz2.4.py
+++ dz2.4.py
@@ -2,19 +2,43 @@
 # Найдите произведение элементов на указанных позициях. 
 # Позиции хранятся в файле file.txt в одной строке одно число.
 
-# Чтение чисел из файла
-f = open("file.txt", "r")
-a, b = f.read().split("\n")
+import random
 
-a, b = int(a)-1, int(b)-1
+def fill_list(n: int) -> list: 
+    new_list = [random.randint(-n, n)]
+    for i in range(1, n):
+        new_list.append(random.randint(-n, n))
+        i += 1
+    return new_list
 
-N = int(input("Введите количество чисел: "))
+def writing_file(k: int, n: int):
+   with open('file.txt', 'w') as position:
+       for i in range(k):
+           position.write(f'{random.randint(0, n-1)}\n')
 
-A = []
+def print_position():
+   path = 'file.txt'
+   position = open(path, 'r')
+   pos_element = []
+   for line in position:
+    pos_element.append(int(line))
+   print(f'Позиции элементов: {pos_element}')
+   position.close()
+   return pos_element
 
-for i in range(-N, N+1):
-    A.append(i)
+def product_elements(user_list: list, k: int) -> int:
+   path = 'file.txt'
+   position = open(path, 'r')
+   product = 1
+   for line in position:
+    product = product * user_list[int(line)]
+   position.close()
+   return product
 
-res = A[a] * A[b]
-
-print(f"Position one: {a+1}\nPosition two: {b+1}\nNumber of elements: {N}\n{A}\n{res}")
+n = int(input('Количество элементов: N = '))
+new_list = fill_list(n)
+k = int(input('Количество множителей: k = '))
+writing_file(k, n)
+print(f'Заданный список: {new_list}')
+print_position()
+print(f'Произведение элементов на заданных позициях равно {product_elements(new_list, k)}')
