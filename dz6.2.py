4a3132f7b1517c36d2d965d1d1228cc036a31b5a
ElenaZamyatina <elenzamyatina12@gmail.com>
2023-01-05 12:32:38 +0400

    dz6 (2,3)

--- dz6.2.py
+++ dz6.2.py
@@ -26,11 +26,11 @@ orel=0
 rejka=0
 while raz<podbr:
     pir=random.randint(1,2)
-raz+=1
-if pir==1:
-    orel+=1
-elif pir==2:
-    rejka+=1
+    raz+=1
+    if pir==1:
+        orel+=1
+    elif pir==2:
+        rejka+=1
 
 print("Орел выпал",orel,"раз")
 
